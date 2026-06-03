f37bb3e77f9ee0038d53ec8053c681a46e9b501c
INGEMINI <mahiagarwal.r1@gmail.com>
2026-06-03 21:18:43 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.8%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -15,17 +15,6 @@ public:
             reverse(matrix[i].begin(),matrix[i].end());
         }
         return matrix;
-    //     int n=matrix.size();
-    //     for(int i=0;i<n;i++){
-    //         for(int j=1+i;j<n;j++){
-    //             swap(matrix[i][j],matrix[j][i]);
-    //         }
-    //     }
-    //     for(int i=0;i<n;i++){
-    //         reverse(matrix[i].begin(),matrix[i].end());
-            
-    //     }
-    //    return matrix;
-        
+    
     }
 };
