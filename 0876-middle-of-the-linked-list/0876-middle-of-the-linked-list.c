e3d97696fcf7b9173c36b6eb691879317455da25
INGEMINI <mahiagarwal.r1@gmail.com>
2026-06-06 18:24:30 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.02%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -7,44 +7,55 @@
  */
 struct ListNode* middleNode(struct ListNode* head) {
 
-int len=0;
-struct ListNode* h1=head;
-
-struct ListNode* h2=head;
-while(h1!=NULL){
-    len++;
-    h1=h1->next;
-
+//tortoise
+struct ListNode* slow = head , *fast = head;
+while((fast!= NULL )&& (fast->next!= NULL)){
+    slow=slow->next;
+    fast=fast->next->next;
+    
 }
-
-int i,x;
-
-if ( (len%2) !=0 ){
-for(i=0;i<len/2;i++){
-
-    h2=h2->next;
-
-}
-x=h2->val;
-}
-
-else{
-for(i=0;i<len/2;i++){
-
-    h2=h2->next;
-
-}
-x=h2->val;
-}
+return slow;
 
 
+//brute
+// int len=0;
+// struct ListNode* h1=head;
+
+// struct ListNode* h2=head;
+// while(h1!=NULL){
+//     len++;
+//     h1=h1->next;
+
+// }
+
+// int i,x;
+
+// if ( (len%2) !=0 ){
+// for(i=0;i<len/2;i++){
+
+//     h2=h2->next;
+
+// }
+// x=h2->val;
+// }
+
+// else{
+// for(i=0;i<len/2;i++){
+
+//     h2=h2->next;
+
+// }
+// x=h2->val;
+// }
 
 
-printf("value is %d",x);
+
+
+// printf("value is %d",x);
 
 
 
 
-return h2;
+// return h2;
 
 }
