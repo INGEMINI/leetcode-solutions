7de2ddafc4d9f5739678f585b1026f0e6b75cf52
INGEMINI <mahiagarwal.r1@gmail.com>
2026-06-07 15:42:49 +0530

    Time: 13 ms (19.21%), Space: 11.8 MB (80.38%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,14 +12,14 @@ public:
         //approach 1-- arr mai hr value daalte chalo agr value repeat ho gyi tbi end and ans is true ya agr head null tk aa gya to false
         //approach i am using here ..tortoise wali
         if((head==NULL)||(head->next==NULL)) return false;
-        ListNode *slow = head , *fast= head->next;
+        ListNode *slow = head , *fast= head;
         
         while((fast!=NULL)&&(fast->next!=NULL)){
-                
-                if (fast==slow) return true;
                 fast = fast->next->next;
                 
                 slow=slow->next;
+                if (fast==slow) return true;
+                
         }        
         
         return false;
