cb35dd26faa3acca8ba75be98c665f2f40a02381
INGEMINI <mahiagarwal.r1@gmail.com>
2026-06-08 12:08:32 +0530

    Time: 99 ms (6.76%), Space: 27.9 MB (97.52%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,19 +4,7 @@ public:
         // mine
         int k = 0, l = 0;
         vector<int> arr(nums.size(), -1);
-        // vector<int> index1(nums1.size(), -1);
-        // vector<int> index2(nums1.size(), -1);
-        // for (int i = 0; i < nums1.size(); i++) {
-        //     for (int j = 0; j < nums2.size(); j++) {
-        //         if (nums1[i] == nums2[j]) {
-        //             index1[k] = i;
-        //             index2[l] = j;
-        //             k++;
-        //             l++;
-        //         }
-        //     }
-        // }
-        int i = 0;
+      
         for (int j = 0 ;j< nums.size();j++) {
             int n = nums.size();
 
@@ -29,7 +17,7 @@ public:
                     
                     break;
                 }
-                i++;
+              
             }
         }
         return arr;
