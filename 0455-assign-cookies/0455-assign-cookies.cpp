a40c9e0da4c2d7a47cc6523295ce0cf0493f2c94
INGEMINI <mahiagarwal.r1@gmail.com>
2026-06-16 10:22:44 +0530

    Time: 9 ms (49.92%), Space: 44.7 MB (77.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     int findContentChildren(vector<int>& g, vector<int>& s) {
+//         int n=g.size();
+//         int m=s.size();
+//         sort(g.begin(),g.end());
+//         sort(s.begin(),s.end());
+//         int l=0,r=0;
+//         while(l<m){
+//             if(r!=n && g[r]<=s[l]){
+//                 r++;
+//             }
+//             l++;
+//         }
+//         return r;
+        
+//     }
+// };
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n=g.size();
-        int m=s.size();
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int l=0,r=0;
-        while(l<m){
-            if(r!=n && g[r]<=s[l]){
-                r++;
+         int count =0;
+    int i=0,j=0;
+    sort(begin(g),end(g));
+    sort(begin(s),end(s));
+         while(i<g.size() && j<s.size())
+         {
+            if(s[j] >= g[i])
+            {
+              count++;
+              i++;
+              j++;
             }
-            l++;
-        }
-        return r;
-        
+            else
+            j++;
+         }
+         return count;
     }
 };
